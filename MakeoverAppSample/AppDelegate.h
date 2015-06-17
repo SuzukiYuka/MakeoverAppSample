3fa368acf7bf4a73eeb388c0507e67e005d0e4b4
SUZUKIyuka <suzukiyuka1218@gmail.com>
2015-06-17 10:56:40 +0900

    Initial Commit

---
+++ MakeoverAppSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MakeoverAppSample
+//
+//  Created by nagata on 6/17/15.
+//  Copyright (c) 2015 nagata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MakeoverAppSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MakeoverAppSample
+//
+//  Created by nagata on 6/17/15.
+//  Copyright (c) 2015 nagata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
